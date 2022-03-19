889273dc727ad76cedad959b455d625d21db8ea2
Emerson Novais <emersonnovais93@gmail.com>
2022-03-19 14:37:36 -0300

    Learning calculate the most cust-benefit fuel to refill
    Complete app

--- cost-benefit/ViewController.swift
+++ cost-benefit/ViewController.swift
@@ -7,13 +7,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension Collection {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
 
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var gasPriceField: UITextField!
+    @IBOutlet weak var alcoholPriceField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func calculateCostBenefit(_ sender: Any) {
+        let gasField = gasPriceField.text
+        let alcoholField = alcoholPriceField.text
+        var condition: Bool
+        
+        condition = validateValues(gasPrice: gasField, alcoholPrice: alcoholField)
+        
+        if condition {
+            resultLabel.text = calculated(gasPrice: gasField!, alcoholPrice: alcoholField!)
+        } else {
+            resultLabel.text = "Digite os preços para ser feito o cálculo"
+        }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func calculated(gasPrice: String, alcoholPrice: String) -> String {
+        let gas = Double(gasPrice)!
+        let alcohol = Double(alcoholPrice)!
+        let costBenefitValue = 0.7
+        let media = alcohol / gas
+        var result: String
+        
+        if (media >= costBenefitValue) {
+            result = "Melhor utilizar Gasolina!"
+        } else {
+            result = "Melhor utilizar Álcool"
+        }
+        
+        return result
+    }
+    
+    func validateValues(gasPrice: String?, alcoholPrice: String?) -> Bool {
+        var condition: Bool
+        var gasCodition = false
+        var alcoholCondition = false
+        
+        if gasPrice!.isNotEmpty {
+            gasCodition = true
+        }
+        if alcoholPrice!.isNotEmpty {
+            alcoholCondition = true
+        }
+        
+        if gasCodition && alcoholCondition {
+            condition = true
+        } else {
+            condition = false
+        }
+        
+        return condition
+    }
 
 }
 
